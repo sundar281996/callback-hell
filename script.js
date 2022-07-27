c92fb604f0a03796e99713f4b7bd74192a4b3e77
sundar281996 <96235096+sundar281996@users.noreply.github.com>
2022-07-27 11:48:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+var i = 10;
+
+setTimeout(function () {
+  document.getElementById("text").innerHTML = i;
+  setTimeout(function () {
+    i--;
+    document.getElementById("text").innerHTML = i;
+    setTimeout(function () {
+      i--;
+      document.getElementById("text").innerHTML = i;
+      setTimeout(function () {
+        i--;
+        document.getElementById("text").innerHTML = i;
+        setTimeout(function () {
+          i--;
+          document.getElementById("text").innerHTML = i;
+          setTimeout(function () {
+            i--;
+            document.getElementById("text").innerHTML = i;
+            setTimeout(function () {
+              i--;
+              document.getElementById("text").innerHTML = i;
+              setTimeout(function () {
+                i--;
+                document.getElementById("text").innerHTML = i;
+                setTimeout(function () {
+                  i--;
+                  document.getElementById("text").innerHTML = i;
+                  setTimeout(function () {
+                    i--;
+                    document.getElementById("text").innerHTML = i;
+                    setTimeout(function () {
+                      i--;
+                      document.getElementById("text").innerHTML =
+                        "Happy Independence Day";
+                    }, 1200);
+                  }, 1200);
+                }, 1200);
+              }, 1200);
+            }, 1200);
+          }, 1200);
+        }, 1200);
+      }, 1200);
+    }, 1200);
+  }, 1200);
+}, 1200);
